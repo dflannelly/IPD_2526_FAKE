66da7ad9f718bcedb96fd740af286be410904c67
dflannelly <110606696+dflannelly@users.noreply.github.com>
2025-11-13 19:44:38 -0500

    Update flan.py

--- flan.py
+++ flan.py
@@ -8,26 +8,13 @@
 
 
 team_name = 'Flan' # Only 10 chars displayed.
-strategy_name = 'Randomness unless you backstab'
-strategy_description = 'If the opponent colludes than the algorithm randomly selects c or b for the next round. If the opponent backstabs, than the algorithm will always backstab back. '
+strategy_name = 'FLANTASTIC'
+strategy_description = 'FLANOMENAL '
 
 import random
 
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
     if len(my_history) == 0:
         return random.choice(['c','b'])
     elif their_history[-1] == 'b':
